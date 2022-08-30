8b57130b0d9683b4b0c8a7f17a0abb00f2436993
NejireSupremacy <JosueChan2006@outlook.com>
2022-08-29 20:24:15 -0500

    Feat: Client status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,11 +1,26 @@
 import { AllEvents, Ready } from "@biscuitland/core";
+import { session } from "../index.js";
 import { Event, WhenType } from "../utils/interfaces.js";
 
-export default class implements Event {
+import { ActivityTypes } from '@biscuitland/api-types';
+
+export class ReadyEvent implements Event {
   name: AllEvents = "ready";
   when: WhenType = "once";
 
   async execute(client: Ready) {
     console.log(`Logged in as ${client.user.username}`);
+
+    const shardsC = session.ws.agent.shards.values();
+    for (const { id } of shardsC) {
+      session.editStatus(id, {
+        activities: [{
+          name: 'a free spirit',
+          type: ActivityTypes.Watching,
+          createdAt: Date.now()
+        }],
+        status: 'online'
+      })
+    }
   }
 }
